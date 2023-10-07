e5f61c81cb2145dd5c2fecbebddffeb4ad004b03
Jade Ye <70247822+jialingye@users.noreply.github.com>
2023-10-07 00:14:57 -0500

    Time: 66 ms (49.08%), Space: 44.4 MB (74%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -3,10 +3,13 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-     for (let i =0; i< nums.length; i++){
-         if(nums[i+1]===nums[i]){
-             nums.splice(i+1, 1);
-             i--;
-         }
-     }
+    let slow = 0, fast = 1
+    while (fast <nums.length){
+        if(nums[slow]!==nums[fast]){
+            slow++
+            nums[slow]=nums[fast]
+        }
+        fast++
+    }
+    return slow+1
 };
