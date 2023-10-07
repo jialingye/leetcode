e4d565090315f7fa22b481eede17bfde7216cb12
Jade Ye <70247822+jialingye@users.noreply.github.com>
2023-10-06 23:57:51 -0500

    Time: 60 ms (60.14%), Space: 44.1 MB (49.44%) - LeetHub

---
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function(head) {
+    if(head === null) return null
+    let slow = head, fast = head.next
+    while(fast !==null){
+        if(fast.val !== slow.val){
+            slow.next = fast;
+            slow = slow.next
+        } 
+        fast = fast.next
+    }
+
+    slow.next = null
+    return head
+};
